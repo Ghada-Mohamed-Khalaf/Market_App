5802d8b9708008ca75e632b032136f11065e0fc5
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-19 22:51:32 +0200

    add card and textFormField

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/core/app_colors.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text(
+            "Welcome To our Market",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Card(
+            color: AppColors.kWhiteColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),),
+            child: Column(
+              children: [
+                TextFormField(decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide:BorderSide(color: AppColors.kBordersideColor),
+
+                  ),
+
+                ),),
+              ],
+            ),
+          ),
+
+        ],
+      )),
+    );
   }
 }
