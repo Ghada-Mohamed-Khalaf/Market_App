c16f9d9650ce67a3374348f9c9568f47fd62b736
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-20 23:23:50 +0200

    finish ui login View

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -8,115 +8,131 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome To our Market",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-          SizedBox(
-            height: 25,
-          ),
-          Card(
-            margin: EdgeInsets.all(24.0),
-            color: AppColors.kWhiteColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
+          child: SingleChildScrollView(
+            child: Column(
+                    children: [
+            SizedBox(
+              height: 20.0,
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  CustomTextFormField(
-                    isSecure: false,
-                    labelText: "Email",
-                    prefIcon: Icon(Icons.email),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  CustomTextFormField(
-                    isSecure: true,
-                    suffIcon: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.visibility_off,
-                      ),
+            Text(
+              "Welcome To our Market",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Card(
+              margin: EdgeInsets.all(24.0),
+              color: AppColors.kWhiteColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      isSecure: false,
+                      labelText: "Email",
+                      prefIcon: Icon(Icons.email),
                     ),
-                    labelText: "Password",
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      CustomTextButton(
-                        text: "Forget Password?",
-                        onTap: () {},
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    CustomTextFormField(
+                      isSecure: true,
+                      suffIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.visibility_off,
+                        ),
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Login",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.kPrimaryColor,
-                            foregroundColor: AppColors.kWhiteColor,
-                          ),
-                          onPressed: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10.0,
-                              vertical: 10.0,
+                      labelText: "Password",
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        CustomTextButton(
+                          text: "Forget Password?",
+                          onTap: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.kPrimaryColor,
+                              foregroundColor: AppColors.kWhiteColor,
                             ),
-                            child: Icon(
-                              Icons.arrow_forward,
-                              color: AppColors.kWhiteColor,
-                            ),
-                          ))
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Login With Google",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold),
-                      ),
-                      CustomArowButton()
-                    ],
-                  ),
-                ],
+                            onPressed: () {},
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                                vertical: 10.0,
+                              ),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: AppColors.kWhiteColor,
+                              ),
+                            ))
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Login With Google",
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
+                        ),
+                        CustomArowButton(
+                          onTap: (){},
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 20.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Already have an account?",style: TextStyle(
+                          fontWeight: FontWeight.bold,fontSize: 15.0,
+                        ),),
+                        SizedBox(width: 7,),
+                        CustomTextButton(text: "Sign up",onTap: (){},),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      )),
+                    ],
+                  ),
+          )),
     );
   }
 }
 
 class CustomArowButton extends StatelessWidget {
-  const CustomArowButton({
-    super.key,
+  CustomArowButton({
+    super.key,required this.onTap,
   });
+  final Function()?onTap;
 
   @override
   Widget build(BuildContext context) {
