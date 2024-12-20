3c53ecb952d27b0faae36e9ff80741b062347130
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-20 12:22:32 +0200

    create and refactor textField in the card loginView

--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -10,6 +10,7 @@ class LoginView extends StatelessWidget {
       body: SafeArea(
           child: Column(
         children: [
+          SizedBox(height: 20.0,),
           Text(
             "Welcome To our Market",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
@@ -18,25 +19,82 @@ class LoginView extends StatelessWidget {
             height: 25,
           ),
           Card(
+            margin:EdgeInsets.all(24.0) ,
             color: AppColors.kWhiteColor,
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(15.0),),
-            child: Column(
-              children: [
-                TextFormField(decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide:BorderSide(color: AppColors.kBordersideColor),
-
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    isSecure: false,
+                    labelText: "Email",
+                    prefIcon: Icon(Icons.email),
                   ),
-
-                ),),
-              ],
+                  SizedBox(height: 20.0,),
+                  CustomTextFormField(
+                    isSecure: true,
+                    suffIcon:IconButton(onPressed: (){}, icon:Icon(Icons.visibility_off ,),) ,
+                    labelText: "Password",
+                  ),
+                ],
+              ),
             ),
           ),
 
         ],
       )),
     );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+   CustomTextFormField({
+    super.key, required this.labelText,this.suffIcon,this.prefIcon,required this.isSecure,
+  });
+ final String labelText;
+ final Widget?suffIcon;
+ final Widget?prefIcon;
+ final bool isSecure;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isSecure ,
+      obscuringCharacter: "*",
+      validator: (value){
+        if(value==null || value.isEmpty){
+          return "This Field is Required";
+        }
+        return null;
+
+      },
+
+      keyboardType: TextInputType.emailAddress,
+
+                  decoration: InputDecoration(
+      labelText: labelText,
+      suffixIcon: suffIcon,
+      prefixIcon: prefIcon,
+
+
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:BorderSide(color: AppColors.kBordersideColor,width: 2.0,),
+
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:BorderSide(color: AppColors.kBordersideColor,width: 2.0,),
+
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:BorderSide(color: AppColors.kBordersideColor,width: 2.0,),
+
+      ),
+
+    ),);
   }
 }
