eb47766ee79d292150c1fd6ba9c6823fbeafbb89
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-21 13:39:04 +0200

    create MainHomeView

---
+++ lib/views/nav_bar/ui/main_home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainHomeView extends StatelessWidget {
+  const MainHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
