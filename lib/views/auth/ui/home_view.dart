1965d002506b66ec83f068d0901acdfac0325de5
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-21 22:08:46 +0200

    create StoreView.dart

--- lib/views/auth/ui/home_view.dart
+++ lib/views/auth/ui/home_view.dart
@@ -5,6 +5,11 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Column(
+      children: [
+        Text("home view"),
+
+      ],
+    );
   }
 }
