ce8c4d10e2be601fddd107f15ecf65347c0059f2
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-24 23:32:01 +0200

    create  and finish Favorite View

--- lib/views/favorite/ui/favorite_view.dart
+++ lib/views/favorite/ui/favorite_view.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/core/components/product_list.dart';
 
 class FavoriteView extends StatelessWidget {
   const FavoriteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("data"),
+    return  Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              "Your Favorite Products",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+
+
+          SizedBox(
+            height: 15.0,
+          ),
+
+
+          ProductList()
+        ],
+      ),
     );
+
   }
 }
