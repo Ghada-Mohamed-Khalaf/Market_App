4d41daca2eed47b55eaa7f751c4f5ba8bfd93e39
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-24 23:26:10 +0200

    create  and finish Store View

--- lib/views/store/ui/store_view.dart
+++ lib/views/store/ui/store_view.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/core/components/Custom_search_field.dart';
+import 'package:market_app/core/components/product_list.dart';
+import 'package:market_app/views/home/ui/widget/categories_list.dart';
+
 
 class StoreView extends StatelessWidget {
   const StoreView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Welcome To Our Market",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              "Welcome To Our Market",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          CustomSearchField(),
+
+
+          SizedBox(
+            height: 15.0,
+          ),
+
+
+          ProductList()
+        ],
+      ),
     );
   }
 }
