8a4195c748d075025db894c34a72de262dde05d3
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-24 23:44:12 +0200

    add card and circleAvatar in profile_view.dart

--- lib/views/profile/ui/profile_view.dart
+++ lib/views/profile/ui/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_app/core/app_colors.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -6,7 +7,25 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("pro"),
+      body: Card(
+        margin: EdgeInsets.all(24.0),
+        color: AppColors.kWhiteColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [ CircleAvatar(
+                radius: 60,
+                backgroundColor: AppColors.kPrimaryColor,
+                foregroundColor:AppColors.kWhiteColor ,
+              )
+
+              ],
+            ),
+        ),
+      ),
     );
   }
 }
