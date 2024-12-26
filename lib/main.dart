9d917feb79f7da48d77bfbb85ef97ab3ca3b53e6
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-26 18:02:52 +0200

    initialize supabase in our project

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ import 'package:market_app/views/favorite/ui/favorite_view.dart';
 import 'package:market_app/views/home/ui/home_view.dart';
 
 import 'package:market_app/views/nav_bar/ui/main_home_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://pdbpghagwrlseewoecse.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkYnBnaGFnd3Jsc2Vld29lY3NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyMDMwMjEsImV4cCI6MjA1MDc3OTAyMX0.oPwXknRn_L3aAQVFMaZURETHJ2uBGk_r5Uut2gK9Bl0',
+  );
+
   runApp(const MyApp());
 }
 
