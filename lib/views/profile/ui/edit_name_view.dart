ff6cb103ec677996499fb750a94502ee45fa316c
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-27 11:46:00 +0200

    create edit name view

---
+++ lib/views/profile/ui/edit_name_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditNameView extends StatelessWidget {
+  const EditNameView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
