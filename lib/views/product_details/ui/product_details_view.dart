25288a2cde4f3ed3c4bec349ce7f194fb7464064
Ghada-Mohamed-Khalaf <gmo625112@gmail.com>
2024-12-28 11:09:37 +0200

    update product_details_view.dart

--- lib/views/product_details/ui/product_details_view.dart
+++ lib/views/product_details/ui/product_details_view.dart
@@ -8,15 +8,59 @@ class ProductDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context,"Product Name"),
+      appBar: buildAppBar(context, "Product Name"),
       body: ListView(
         children: [
-          CachedImage(url:  "https://img.freepik.com/premium-psd/headset-product-social-media-post-banner-template_448714-340.jpg?ga=GA1.1.328080625.1734945837&semt=ais_hybrid",),
+          CachedImage(
+            url:
+                "https://img.freepik.com/premium-psd/headset-product-social-media-post-banner-template_448714-340.jpg?ga=GA1.1.328080625.1734945837&semt=ais_hybrid",
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10,),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Product Name"),
+                    Text("123 LE"),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                    children: [
+                      Text("3 "),
+                      Icon(Icons.star,color: Colors.amber),
+                    ],
 
+                  ),
+                    Icon(Icons.favorite,color: Colors.grey,),
+
+
+                  ],
+                ),
+                SizedBox(height: 10,),
+                Text("books desc"),
+                SizedBox(height: 10,),
+                Row(
+                  children: [
+                    Icon(Icons.star,color: Colors.amber,),
+                    Icon(Icons.star,color: Colors.amber,),
+                    Icon(Icons.star,color: Colors.amber,),
+                    Icon(Icons.star,color: Colors.grey,),
+                    Icon(Icons.star,color: Colors.grey,),
+
+
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
-
     );
   }
 }
-
